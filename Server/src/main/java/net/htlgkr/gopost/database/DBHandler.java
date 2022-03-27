099fce7612954b160792b237889ef69339527bc6
Schatzgoblin <Kammererm190070@sus.htl-grieskirchen.at>
2022-03-27 21:59:58 +0200

    Replaced XML with Database

--- Server/src/main/java/net/htlgkr/gopost/database/DBHandler.java
+++ Server/src/main/java/net/htlgkr/gopost/database/DBHandler.java
@@ -25,15 +25,8 @@ public class FileHandler {
     }
 
     public void writeIntoFile() {
-        for (FileObject entry : entries) {
-            if (entry.getObject() == null) continue;
-            if (entry.isEncrypted() && entry.getObject() instanceof String string) {
-                xmlEncoder.writeObject(Encrypt.SHA512(string));
-            } else {
-                xmlEncoder.writeObject(entry.getObject());
-            }
-            xmlEncoder.flush();
-        }
+
+    }
 //        try {
 //            XMLEncoder writer = new XMLEncoder(new FileOutputStream("fileName"));
 //            writer.setPersistenceDelegate(LocalDate.class,
@@ -53,33 +46,12 @@ public class FileHandler {
 //        } catch (FileNotFoundException e) {
 //            e.printStackTrace();
 //        }
-    }
 
     public Object readFromFile(FileObject readObject) {
-        try (XMLDecoder xmlDecoder = new XMLDecoder(new FileInputStream(filename))) {
-            Object object;
-            while ((object = xmlDecoder.readObject()) != null) {
-                if (readObject.isEncrypted()) {
-                    if (Encrypt.SHA512(String.valueOf(readObject.getObject())).equals(object)) {
-                        return object;
-                    }
-                } else {
-                    if (object.equals(readObject.getObject())) {
-                        return object;
-                    }
-                }
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
         return null;
     }
 
     public void removeFromFile(FileObject remove) {
-        if (remove.isEncrypted()) {
-            xmlEncoder.remove(Encrypt.SHA512((String) remove.getObject()));
-        } else {
-            xmlEncoder.remove(remove.getObject());
-        }
+
     }
 }
