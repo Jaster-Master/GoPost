6b187fd3314df923ca722e7fa5121246bef72901
Schatzgoblin <Kammererm190070@sus.htl-grieskirchen.at>
2022-03-30 18:52:00 +0200

    pulling

--- Server/src/main/java/net/htlgkr/gopost/server/Main.java
+++ Server/src/main/java/net/htlgkr/gopost/server/Main.java
@@ -13,22 +13,7 @@ public class Main {
     public static Server runningServer;
 
     public static void main(String[] args) {
-        DBHandler dbHandler = new DBHandler();
-
-        String name = "David";
-        name.split(";");
-        String profName = "dk";
-        String email = "Kogler@nom.at";
-        String password = "nomnom";
-        boolean isPrivate = false;
-        LocalDateTime localDateTime = LocalDateTime.now();
-        Timestamp timestamp = Timestamp.valueOf(localDateTime);
-        dbHandler.executeStatementsOnDB("INSERT INTO GoUser(GoUserName,GoProfileName,GoUserEmail,GoUserPassword,GoUserIsPrivate,GoUserDateTime) VALUES(?,?,?,?,?,?)", name, profName, email, password, isPrivate, timestamp);
-        List<DBObject> list = dbHandler.readFromDB("Select * FROM GoUser WHERE GoUserId = ?",  "1","1;BigInt", "2;String", "3;String");
-        for (DBObject o : list) {
-            System.out.println(o.getObject().toString());
-        }
-        //runningServer = new Server();
-        //runningServer.startServer(args);
+        runningServer = new Server();
+        runningServer.startServer(args);
     }
 }
