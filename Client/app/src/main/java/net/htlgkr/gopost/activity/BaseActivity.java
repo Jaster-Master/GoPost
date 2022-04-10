d7da410a0cfb23a7bcf72bf52b51ba108b72bc9a
JasterXYZ <ZecherJ190044@sus.htl-grieskirchen.at>
2022-04-10 16:20:01 +0200

    Added requestPermission method in BaseActivity

--- Client/app/src/main/java/net/htlgkr/gopost/activity/BaseActivity.java
+++ Client/app/src/main/java/net/htlgkr/gopost/activity/BaseActivity.java
@@ -1,9 +1,12 @@
 package net.htlgkr.gopost.activity;
 
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
 public class BaseActivity extends AppCompatActivity {
 
@@ -28,5 +31,16 @@ public class BaseActivity extends AppCompatActivity {
     protected void onSaveInstanceState(@NonNull Bundle outState) {
         super.onSaveInstanceState(outState);
         outState.putBoolean("IsDarkMode", true);
+    }
+
+    public void requestPermission(String permission, int requestCode) {
+        if (ContextCompat.checkSelfPermission(this, permission) == PackageManager.PERMISSION_DENIED) {
+            ActivityCompat.requestPermissions(this, new String[]{permission}, requestCode);
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
     }
 }
