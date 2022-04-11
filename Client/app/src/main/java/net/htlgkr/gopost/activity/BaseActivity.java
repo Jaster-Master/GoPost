0879e23ec450d94a09dfe4b65d85a24d99b55655
gschwendtnerj <25jakobchima@gmail.com>
2022-04-11 08:29:12 +0200

    pulling

--- Client/app/src/main/java/net/htlgkr/gopost/activity/BaseActivity.java
+++ Client/app/src/main/java/net/htlgkr/gopost/activity/BaseActivity.java
@@ -1,14 +1,21 @@
 package net.htlgkr.gopost.activity;
 
 import android.os.Bundle;
+import android.util.Log;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
-public class BaseActivity extends AppCompatActivity {
+import net.htlgkr.gopost.R;
+import net.htlgkr.gopost.fragments.HomeFragment;
+import net.htlgkr.gopost.fragments.HomeFragmentListener;
+
+public class BaseActivity extends AppCompatActivity implements HomeFragmentListener {
 
     public static String tag;
     public static BaseActivity instance;
+    private HomeFragment homeFragment;
+    private final static String LOG = BaseActivity.class.getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,6 +23,10 @@ public class BaseActivity extends AppCompatActivity {
         instance = this;
         tag = instance.getClass().getSimpleName();
         setTheme(savedInstanceState);
+        setContentView(R.layout.template_main); //TODO: Change from template to actual xml
+
+        homeFragment = new HomeFragment();
+        getSupportFragmentManager().beginTransaction().replace(R.id.homeFragmentContainer,homeFragment).commit();
     }
 
     private void setTheme(Bundle savedInstanceState) {
@@ -28,5 +39,23 @@ public class BaseActivity extends AppCompatActivity {
     protected void onSaveInstanceState(@NonNull Bundle outState) {
         super.onSaveInstanceState(outState);
         outState.putBoolean("IsDarkMode", true);
+    }
+
+
+    @Override
+    public void onInputSent(String input) {
+        Log.i(LOG,"Received input from HomeFragment: "+input);
+        switch(input)
+        {
+            case "Search":
+                //TODO Switch to SearchActivity
+                break;
+            case "Home":
+                //TODO Switch to HomeActivity
+                break;
+            case "Profile":
+                //TODO Switch to ProfileActivity
+                break;
+        }
     }
 }
---
+++ Client/app/src/main/java/net/htlgkr/gopost/fragments/HomeFragment.java
@@ -0,0 +1,50 @@
+package net.htlgkr.gopost.fragments;
+
+import android.content.Context;
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import net.htlgkr.gopost.R;
+
+
+public class HomeFragment extends Fragment {
+
+    private HomeFragmentListener listener;
+    private ImageView imageViewSearch;
+    private ImageView imageViewHome;
+    private ImageView imageViewProfile;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_home, container, false);
+        imageViewSearch = view.findViewById(R.id.imageViewSearch);
+        imageViewHome = view.findViewById(R.id.imageViewHome);
+        imageViewProfile = view.findViewById(R.id.imageViewProfile);
+
+        imageViewSearch.setOnClickListener(v -> listener.onInputSent("Search"));
+        imageViewHome.setOnClickListener(v -> listener.onInputSent("Home"));
+        imageViewProfile.setOnClickListener(v -> listener.onInputSent("Profile"));
+        return view;
+    }
+
+    @Override
+    public void onAttach(Context context)
+    {
+        super.onAttach(context);
+        if(context instanceof HomeFragmentListener) {
+            listener = (HomeFragmentListener) context;
+        }
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+        listener = null;
+    }
+}
